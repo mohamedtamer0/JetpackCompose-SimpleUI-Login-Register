53ea2df523eac8c562166ac78c27b58c267a0296
mohamedtamer0 <mt4452242@gmail.com>
2021-12-09 03:27:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/simpleui_login_register_jetpackcompose/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.simpleui_login_register_jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.simpleui_login_register_jetpackcompose.ui.theme.SimpleUILoginRegisterJetpackComposeTheme
+import com.example.simpleui_login_register_jetpackcompose.view.LoginPage
+import com.example.simpleui_login_register_jetpackcompose.view.Register
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleUILoginRegisterJetpackComposeTheme {
+                navigatePage()
+            }
+        }
+    }
+}
+
+@Composable
+fun navigatePage() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login_page",
+        builder = {
+            composable("login_page", content = { LoginPage(navController = navController) })
+            composable("register_page", content = { Register(navController = navController) })
+        }
+    )
+}
